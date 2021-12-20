5c0987cd1977bacb3f8539b32b9cd90a3d4127d7
Carlos <carloszapata91@gmail.com>
2021-12-20 15:12:47 -0500

    Nuevos componentes
    Componentes: productos, contactanos, nuestros-productos

---
+++ FrontEnd/src/app/nuestros-productos/nuestros-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuestros-productos',
+  templateUrl: './nuestros-productos.component.html',
+  styleUrls: ['./nuestros-productos.component.scss']
+})
+export class NuestrosProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
