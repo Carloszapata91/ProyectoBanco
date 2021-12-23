99595517a44f07681886f31a4f71d9cdc9c1b5ad
Carlos <carloszapata91@gmail.com>
2021-12-23 10:04:19 -0500

    Mejora final de las rutas
    Mejora rutas de los componentes registro e inicio

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import {ReactiveFormsModule} from '@angular/forms'
 import { ContactoComponent } from './screens/contacto/contacto.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'dashboard', pathMatch:'full' },
--- FrontEnd/src/app/screens/dashboard/dashboard-routing.module.ts
+++ FrontEnd/src/app/screens/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,8 @@ import { CarProductsComponent } from '../car-products/car-products.component';
 import { LoginComponent } from '../login/login.component';
 import{ HomeComponent } from '../home/home.component';
 import { ContactoComponent } from '../contacto/contacto.component';
+import { UsuarioComponent } from 'src/app/usuario/usuario.component';
+import { RegistroComponent } from '../registro/registro.component';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -13,7 +15,9 @@ const routes: Routes = [
      {path:'',redirectTo:'inicio',pathMatch:'full'},
      {path:'inicio',component:HomeComponent},
      {path:'login',component:LoginComponent},
-     {path:'contacto',component:ContactoComponent}
+     {path:'contacto',component:ContactoComponent},
+     {path:'usuario',component:UsuarioComponent},
+     {path:'registro',component:RegistroComponent}
    ]},
    {path:'card-product',component:CarProductsComponent}
 ];
