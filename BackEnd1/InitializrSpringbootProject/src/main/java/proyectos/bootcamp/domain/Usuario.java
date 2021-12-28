ab463b13bf6c9a69bc7a64061a4007ad56856c59
Carlos <carloszapata91@gmail.com>
2021-12-28 10:37:10 -0500

    Continuacion clase entidad usuario

--- BackEnd1/InitializrSpringbootProject/src/main/java/proyectos/bootcamp/domain/Usuario.java
+++ BackEnd1/InitializrSpringbootProject/src/main/java/proyectos/bootcamp/domain/Usuario.java
@@ -1,6 +1,7 @@
 
 package proyectos.bootcamp.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.*;
 import javax.persistence.Table;
@@ -12,8 +13,14 @@ import lombok.Data;
  */
 @Data   //Genera todos los Getter y Setter de los atributos de la clase
 @Entity
-@Table(name="usuario")
-public class Usuario {
+@Table(name="usuario")  //Para evitar problemas entre el nombre de la tabla en base de datos (usuario) y el nombre de la clase (Usuario)
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID= 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long  idUsuario;
     private String nombre;
     private String apellidos;
     private String tipoIdentificacion; 
